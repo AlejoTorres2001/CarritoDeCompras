195c9302f68f8f5c87437ce96f51915af5559921
T0w3r <torres.alejo2001@gmail.com>
2022-03-17 14:20:42 -0300

    metodos basicos

--- Carrito.py
+++ Carrito.py
@@ -1,1 +1,15 @@
-print("Carrito")
+class Carrito():
+    def __init__(self):
+        self.productos = []
+        self.total = 0
+        self.cantidad = 0
+    def agregar(self, producto):
+        self.productos.append(producto)
+        self.total += producto.precio
+        self.cantidad += 1
+    def quitar(self, producto):
+        self.productos.remove(producto)
+        self.total -= producto.precio
+        self.cantidad -= 1
+    def __str__(self):
+        return "Carrito: " + str(self.productos) + " Total: " + str(self.total) + " Cantidad: " + str(self.cantidad)

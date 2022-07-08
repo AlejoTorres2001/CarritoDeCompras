593fd34252ca7d4fe3f0dc011c1ac187a985b432
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-07-08 10:58:27 -0300

    #1 un poco de ayuda

--- Carrito.py
+++ Carrito.py
@@ -3,25 +3,34 @@ class Carrito():
         self.productos = []
         self.total = 0
         self.cantidad_total = 0
-        
-    def agregar(self, producto,cantidad=1):
-        self.productos.append([producto,cantidad])
+
+    def agregar(self, producto, cantidad=1):
+        if producto.stock == 0:  # Si el producto no tiene stock
+            print(f'No hay stock de producto {producto.nombre}')
+            return
+        if producto.stock < cantidad:  # Si la cantidad de productos que se quiere agregar es mayor a la cantidad de stock
+            print(f'No hay suficiente stock de producto {producto.nombre}')
+            return
+        #! Y si no quiero permitir que existan mas unidades en el carrito de un producto que el stock?
+        self.productos.append([producto, cantidad])
         self.total += producto.precio*cantidad
         self.cantidad_total += cantidad
-        
+
     def quitar(self, producto):
         for prod in self.productos:
-          if prod[0].nombre == producto.nombre:
-            if prod[1] -1 == 0:
-              self.productos.remove(prod)
-              self.cantidad_total -= 1  
-              self.total -= prod[0].precio
-            else:
-              prod[1] -= 1
-              self.cantidad_total -= 1
-            
+            if prod[0].nombre == producto.nombre:
+                if prod[1] - 1 == 0:
+                    self.productos.remove(prod)
+                    self.cantidad_total -= 1
+                    self.total -= prod[0].precio
+                else:
+                    prod[1] -= 1
+                    self.cantidad_total -= 1
+                return
+        print(f'No se encontro el producto a eliminar: {producto.nombre}')
+
     def __str__(self):
-        prods=""
+        prods = ""
         for prod in self.productos:
-          prods+=f"Nombre:{prod[0].nombre} Precio: {str(prod[0].precio)} Cantidad: {str(prod[1]) } \n"
+            prods += f"Nombre:{prod[0].nombre} Precio: {str(prod[0].precio)} Cantidad: {str(prod[1]) } \n"
         return "Carrito: \n" + prods + " Total A Pagar: " + str(self.total) + " Cantidad de productos: " + str(self.cantidad_total)
--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from Carrito import Carrito
 from Producto import Producto
 if __name__ == '__main__':
-  cart = Carrito()
-  
-  prod1 = Producto("Pizza", precio=10, stock=10)
-  prod2 = Producto("Hamburguesa", precio=5, stock=10)
-  
-  cart.agregar(prod1,2)
-  cart.agregar(prod2)
-  print(cart)
-  cart.quitar(prod1)
-  print(cart)
-  
+    cart = Carrito()
+    #!Probemos con un solo articulo
+    prod1 = Producto("Pizza", precio=10, stock=3)
+    #prod2 = Producto("Hamburguesa", precio=5, stock=10)
+    #! Tratamos de agregar mas que el stock permitido
+    cart.agregar(prod1, 4)
+    # cart.agregar(prod2)
+    print(cart)
+    cart.quitar(prod1)
+    print(cart)
